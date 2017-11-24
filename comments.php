699c69cb0a0f3311804895dd9c738f623dec9421
SpdyGonzales <erik.gholm@gmail.com>
2017-11-24 16:30:04 +0100

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,16 +1,23 @@
 <?php 
 session_start(); 
 include "comments.inc.php";
-?>
+?>     
        <div class="comments--wrapper">
         <h2>Comment section - Give your feedback!</h2>
         <?php
-          echo"<form method='POST' action='".setComments($conn)."#comments'>
+          echo"<form method='POST' action=''>
             <input type='hidden' name='uid' value='".$_SESSION['uid']."'>
             <input type='hidden' name='date' value='".date('Y-m-d H:i:s')."'>
             <textarea name='message'></textarea>
             <input type='submit' name='commentSubmit'>
           </form>";
-          getComments($conn);
+          if(!isset($_POST['commentSubmit'])){
+              getComments($conn, $recipesite);
+          }
+        ?>
+        <?php
+        if(isset($_POST['commentSubmit'])){
+          setComments($conn, $recipesite);
+        }
         ?>
        </div>
